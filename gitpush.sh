275650cad463da80a4332bf86f9eeb485eebddb7
someus <sunlt1699@gmail.com>
2013-12-04 23:48:47 +0800

    ...

---
+++ gitpush.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#should be run at the root of this repo
+
+echo ">>git add ."
+git add .
+echo ""
+
+echo ">>git commit -m \"...\""
+git commit -m "..."
+echo ""
+
+echo ">>git merge master"
+git merge master
+echo ""
+
+echo ">>git push origin master"
+git push origin master
+echo ""
